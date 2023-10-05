db6565bcd20c565b64e3396c505b7edf946f2126
Humairaa1 <humaira71bd@gmail.com>
2023-10-06 00:12:59 +0600

    header complete with update date from moment js

--- src/shared/header/Header.jsx
+++ src/shared/header/Header.jsx
@@ -1,8 +1,14 @@
+import logo from '../../assets/photos/logo.png'
+import moment from 'moment';
 
 const Header = () => {
     return (
-        <div>
-            <h1 className="text-3xl">header</h1>
+        <div className='mt-12 text-center'>
+            <img className='mx-auto mb-5' src={logo} alt="" />
+            <p>Journalism Without Fear or Favour</p>
+            <p className='text-xl font-medium'>
+            {moment().format("dddd, MMMM D, YYYY")}
+            </p>
         </div>
     );
 };
