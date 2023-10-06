2367ab1a5b65c17014de085170b3c6f406fa3175
Humairaa1 <humaira71bd@gmail.com>
2023-10-06 12:45:00 +0600

    data lode and categories name show in leftSideNAv

--- src/shared/leftSideNav/LeftSideNav.jsx
+++ src/shared/leftSideNav/LeftSideNav.jsx
@@ -1,8 +1,29 @@
 
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftSideNav = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
+
     return (
         <div>
-            <h1 className="text-3xl">left side</h1>
+            <h1 className="text-xl font-semibold mb-8">All Category</h1>
+            <div className="space-y-4">
+                {
+                    categories.map(category =>
+                        <li key={category.id}
+                            className="list-none text-xl text-slate-400 font-medium pl-8 hover:bg-slate-100 hover:text-black p-3 rounded-md">
+                            <Link>{category.name}</Link>
+                        </li>)
+                }
+            </div>
         </div>
     );
 };
